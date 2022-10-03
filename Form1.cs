54b7bf617538edf3f8e8b4ba0e57d322c4f102cc
brenekv.03 <brenekv.03@A321-15>
2022-10-03 12:23:57 +0200

    První button

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,19 @@ namespace datoveSouboryRozhrani00
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("celaCisla.dat", FileMode.Create,FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            for (int i = 0; i < textBox1.Lines.Count(); ++i)
+            {
+                int cislo = int.Parse(textBox1.Lines[i]);
+                bw.Write(cislo);
+            }
+            bw.Close();
+            fs.Close();
         }
     }
 }
